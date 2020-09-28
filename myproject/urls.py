c2f71bd5289a0371f2c3f9d13427febf052188d6
Harsh-Sanklecha <sanklecha.harsh@gmail.com>
2020-09-29 02:16:35 +0530

    Added next 2.5 pages

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,6 +5,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('doctors/', views.doctors, name ="doctors")
+    path('doctors/', views.doctors, name ="doctors"),
+    path('consult/', views.consult, name ="consult"),
+    path('diagnosis/', views.diagnosis, name ="diagnosis"),
+    path('vijaya/', views.personalDiag, name ="personalDiag"),
 
 ]
--- myproject/views.py
+++ myproject/views.py
@@ -6,3 +6,12 @@ def home(request):
 
 def doctors(request):
     return render(request, 'myproject/doctors.html')
+
+def consult(request):
+    return render(request, 'myproject/consult.html')
+
+def diagnosis(request):
+    return render(request, 'myproject/diagnosis.html')
+
+def personalDiag(request):
+    return render(request, 'myproject/personal_diag.html')
